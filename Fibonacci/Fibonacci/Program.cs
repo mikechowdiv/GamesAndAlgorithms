1eec0f3dc3810c3da1b328c2d7bf17aa9ff5ee85
Mike Chow <mikechowdiv@gmail.com>
2017-12-12 21:27:57 +1100

    update

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n1 = 0;
+            var n2 = 1;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+
+            for (int i = 0; i <= 15; i++)
+            {
+                var next = n1 + n2;
+                n1 = n2;
+                n2 = next;
+                Console.WriteLine(next);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Fibonacci recusive solution with return value");
+            for (int i = 0; i <= 15; i++)
+            {
+                Console.WriteLine(Recursion(i));
+            }
+        }
+
+        private static int Recursion(int n)
+        {
+            if (n <= 1) return 1;
+            return Recursion(n - 2) + Recursion(n - 1);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Loop Solution");
+            string text;
+
+            for (int i = 100; i >= 1; i--)
+            {
+                if (i % 15 == 0)
+                    text = "FizzBuzz";
+                else if (i % 3 == 0)
+                    text = "Fizz";
+                else if (i % 5 == 0) 
+                    text = "Buzz";
+                else
+                    text = i.ToString();
+
+                Console.WriteLine(text);
+            }
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Recursive Solution: ");
+            Recursion(1);
+        }
+
+        private static void Recursion(int n)
+        {
+            string text = "";
+            if (n > 100) return;
+
+            if(n % 15 == 0)
+                text = "FizzBuzz";
+            else if (n % 3 == 0)
+                text = "Fizz";
+            else if (n % 5 == 0)
+                text = "Buzz";
+            else
+                text = n.ToString();
+
+            Console.WriteLine(text);
+            Recursion(n + 1);
+        }
+
+    }
+}
